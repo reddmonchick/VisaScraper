942f24ddc52559dcc736ee1fa6673b3f2884f5c1
reddmonchick <reddmonchick375@gmail.com>
2025-09-22 07:58:25 +0300

    update

--- src/visascraper/utils/logger.py
+++ src/visascraper/utils/logger.py
@@ -1,23 +1,19 @@
 import logging
 import os
 
-# Создаем папку logs, если её нет
 os.makedirs("logs", exist_ok=True)
 
-# Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - [%(module)s] %(message)s',
     handlers=[
-        logging.StreamHandler(),  # Логи в консоль
-        logging.FileHandler("logs/app.log", encoding='utf-8')  # Логи в файл
+        logging.StreamHandler(),  
+        logging.FileHandler("logs/app.log", encoding='utf-8')  
     ]
 )
 
-# Получаем логгер
 logger = logging.getLogger(__name__)
 
-# Отключаем подробные логи от библиотек
 logging.getLogger("google.auth.transport").setLevel(logging.WARNING)
 logging.getLogger("google.auth.client").setLevel(logging.WARNING)
 logging.getLogger("oauth2client").setLevel(logging.WARNING)
